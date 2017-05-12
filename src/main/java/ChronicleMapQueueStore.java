016690d5985b9ca38e991a92e67509c3bcf979d0
Arnold Franke <franke@synyx.de>
2017-05-12 13:06:23 +0200

    implement persistence for hazelcast queue

---
+++ src/main/java/ChronicleMapQueueStore.java
@@ -0,0 +1,57 @@
+import com.hazelcast.core.QueueStore;
+import net.openhft.chronicle.map.ChronicleMap;
+import net.openhft.chronicle.map.ChronicleMapBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ChronicleMapQueueStore implements QueueStore<String> {
+
+    ChronicleMap<Long, String> chronicleMap;
+
+    public ChronicleMapQueueStore() {
+        try {
+            final File file = new File("chroniclemapstore.dat");
+            ChronicleMapBuilder<Long, String> builder = ChronicleMapBuilder.of(Long.class, String.class);
+            chronicleMap = builder.entries(1000L).averageValue("xxx").createPersistedTo(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void store(Long aLong, String s) {
+        chronicleMap.put(aLong, s);
+    }
+
+    public void storeAll(Map<Long, String> map) {
+        for (Long aLong : map.keySet()) {
+            chronicleMap.put(aLong, map.get(aLong));
+        }
+    }
+
+    public void delete(Long aLong) {
+        chronicleMap.remove(aLong);
+    }
+
+    public void deleteAll(Collection<Long> collection) {
+        for (Long aLong : collection) {
+            chronicleMap.remove(aLong);
+        }
+    }
+
+    public String load(Long aLong) {
+        return chronicleMap.get(aLong);
+    }
+
+    public Map<Long, String> loadAll(Collection<Long> collection) {
+        return new HashMap<Long, String>(chronicleMap);
+    }
+
+    public Set<Long> loadAllKeys() {
+        return null;
+    }
+}
--- src/test/java/BugTest.java
+++ src/test/java/BugTest.java
@@ -1,5 +1,6 @@
 import com.hazelcast.config.Config;
 import com.hazelcast.config.QueueConfig;
+import com.hazelcast.config.QueueStoreConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IQueue;
@@ -7,32 +8,47 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 
 public class BugTest {
 
     @Test
     public void bugTest() {
+
+        // configure queuestore
+        QueueStoreConfig queueStoreConfig = new QueueStoreConfig();
+        queueStoreConfig.setEnabled(true);
+        Properties properties = new Properties();
+        properties.put("binary", false);
+        properties.put("memory-limit", 1000);
+        properties.put("bulk-load", 4);
+        queueStoreConfig.setStoreImplementation(new ChronicleMapQueueStore());
+        queueStoreConfig.setProperties(properties);
+
+        // configure queue
         QueueConfig queueConfig = new QueueConfig();
         queueConfig.setName("buggyQueue");
+        queueConfig.setQueueStoreConfig(queueStoreConfig);
 
+        // configure hazelcast instance
         Config hzconfig = new Config();
         hzconfig.setInstanceName("buggyInstance");
         hzconfig.addQueueConfig(queueConfig);
-
         HazelcastInstance instance = Hazelcast.newHazelcastInstance();
 
+        // this is where the test begins
         IQueue<String> queue = instance.getQueue("buggyQueue");
-
-        queue.offer("01");
-        queue.offer("02");
+        int i = 0;
+        while(i < 10) {
+            queue.offer(String.valueOf(i));
+            i++;
+        }
 
         List<String> drained = new ArrayList<String>();
 
-        queue.drainTo(drained, 1);
+        queue.drainTo(drained, 4);
 
-        assert drained.size() == 2;
+        assert drained.size() == 4;
     }
-
-
 }
